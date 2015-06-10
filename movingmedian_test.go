8c1c46a62214484572f4c2d9d49e12dcf8c87c18
Jader Dias <jader.dias@booking.com>
2015-06-10 19:02:56 +0200

    replaced random tests by unit tests, highlighting the existing bug that was introduced in the commit f7aeb19829d9190a38a0d66c26afe26e5fd7268b

--- movingmedian_test.go
+++ movingmedian_test.go
@@ -1,23 +1,45 @@
 package movingmedian
 
 import (
-	"github.com/wangjohn/quickselect"
-	"math"
 	"math/rand"
 	"testing"
 )
 
-func TestMedian(t *testing.T) {
-	var windowSize = 10
-	data := getData(100)
-	m := NewMovingMedian(windowSize)
-	for i, v := range data {
-		want := median(data, i, windowSize)
+func TestSameNumberInBothHeaps(t *testing.T) {
+	tests := []struct {
+		name       string
+		windowSize int
+		data       []float64
+		want       []float64
+	}{
+		{
+			"OddWindowSize",
+			3,
+			[]float64{1, 3, 5, 7, 9, 11},
+			[]float64{1, 2, 3, 5, 7, 9},
+		},
+		{
+			"EvenWindowSize",
+			4,
+			[]float64{1, 3, 5, 7, 9, 11},
+			[]float64{1, 2, 3, 4, 6, 8},
+		},
+		{
+			"SameNumberInBothHeaps",
+			4,
+			[]float64{11, 13, 13, 13, 25, 27, 29, 31},
+			[]float64{11, 12, 13, 13, 13, 19, 26, 28},
+		},
+	}
 
-		m.Push(v)
-		actual := m.Median()
-		if want != actual {
-			t.Errorf("median failed on index %v: item %v want %v actual %v", i, v, want, actual)
+	for _, test := range tests {
+		m := NewMovingMedian(test.windowSize)
+		for i, v := range test.data {
+			m.Push(v)
+			actual := m.Median()
+			if test.want[i] != actual {
+				t.Errorf("failed on test %s index %d the median of %f is %f and not %f", test.name, i, test.data[1+i-test.windowSize:1+i], test.want[i], actual)
+			}
 		}
 	}
 }
@@ -60,42 +82,4 @@ func getData(rangeSize int) []float64 {
 	}
 
 	return data
-}
-
-func median(data []float64, i, windowSize int) float64 {
-	min := 1 + i - windowSize
-	if min < 0 {
-		min = 0
-	}
-
-	window := make([]float64, 1+i-min)
-	copy(window, data[min:i+1])
-	return percentile(window, 50, true)
-}
-
-func percentile(data []float64, percent float64, interpolate bool) float64 {
-	if len(data) == 0 || percent < 0 || percent > 100 {
-		return math.NaN()
-	}
-	if len(data) == 1 {
-		return data[0]
-	}
-
-	k := (float64(len(data)-1) * percent) / 100
-	length := int(math.Ceil(k)) + 1
-	quickselect.Float64QuickSelect(data, length)
-	top, secondTop := math.Inf(-1), math.Inf(-1)
-	for _, val := range data[0:length] {
-		if val > top {
-			secondTop = top
-			top = val
-		} else if val > secondTop {
-			secondTop = val
-		}
-	}
-	remainder := k - float64(int(k))
-	if remainder == 0 || !interpolate {
-		return top
-	}
-	return (top * remainder) + (secondTop * (1 - remainder))
 }
