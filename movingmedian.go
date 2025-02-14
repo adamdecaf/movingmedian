789527b99057cf1945d80532883b2cdeec3c4e31
Adam Shannon <adamkshannon@gmail.com>
2025-02-14 12:51:38 -0600

    feat: major performance improvement
    Compared to JaderDias implementation
    
                                 │  before.txt  │              latest2.txt               │
                                 │    sec/op    │    sec/op      vs base                 │
    _10values_windowsize1-16       148.5n ± ∞ ¹    211.9n ± ∞ ¹        ~ (p=1.000 n=1) ²
    _100values_windowsize10-16     3.757µ ± ∞ ¹    2.553µ ± ∞ ¹        ~ (p=1.000 n=1) ²
    _10Kvalues_windowsize100-16    852.5µ ± ∞ ¹   1056.6µ ± ∞ ¹        ~ (p=1.000 n=1) ²
    _10Kvalues_windowsize1000-16   1.200m ± ∞ ¹    1.852m ± ∞ ¹        ~ (p=1.000 n=1) ²
    geomean                        27.48µ          32.08µ        +16.71%
    
                                 │  before.txt   │              latest2.txt               │
                                 │     B/op      │     B/op       vs base                 │
    _10values_windowsize1-16        208.00 ± ∞ ¹     16.00 ± ∞ ¹        ~ (p=1.000 n=1) ²
    _100values_windowsize10-16       592.0 ± ∞ ¹     160.0 ± ∞ ¹        ~ (p=1.000 n=1) ²
    _10Kvalues_windowsize100-16    3.922Ki ± ∞ ¹   1.750Ki ± ∞ ¹        ~ (p=1.000 n=1) ²
    _10Kvalues_windowsize1000-16   34.42Ki ± ∞ ¹   16.00Ki ± ∞ ¹        ~ (p=1.000 n=1) ²
    geomean                        1.995Ki           523.6        -74.37%
    
                                 │  before.txt  │             latest2.txt              │
                                 │  allocs/op   │  allocs/op   vs base                 │
    _10values_windowsize1-16        3.000 ± ∞ ¹   2.000 ± ∞ ¹        ~ (p=1.000 n=1) ²
    _100values_windowsize10-16     11.000 ± ∞ ¹   2.000 ± ∞ ¹        ~ (p=1.000 n=1) ²
    _10Kvalues_windowsize100-16    17.000 ± ∞ ¹   2.000 ± ∞ ¹        ~ (p=1.000 n=1) ²
    _10Kvalues_windowsize1000-16   23.000 ± ∞ ¹   2.000 ± ∞ ¹        ~ (p=1.000 n=1) ²
    geomean                         10.66         2.000        -81.23%
    
    cpu: Intel(R) Core(TM) i9-9880H CPU @ 2.30GHz
    Benchmark_10values_windowsize1
    Benchmark_10values_windowsize1-16        	 5293174	       211.9 ns/op	      16 B/op	       2 allocs/op
    Benchmark_100values_windowsize10
    Benchmark_100values_windowsize10-16      	  439552	      2553 ns/op	     160 B/op	       2 allocs/op
    Benchmark_10Kvalues_windowsize100
    Benchmark_10Kvalues_windowsize100-16     	    1075	   1056622 ns/op	    1792 B/op	       2 allocs/op
    Benchmark_10Kvalues_windowsize1000
    Benchmark_10Kvalues_windowsize1000-16    	     622	   1851737 ns/op	   16384 B/op	       2 allocs/op
    PASS
    ok  	github.com/adamdecaf/movingmedian	5.466s

--- movingmedian.go
+++ movingmedian.go
@@ -1,166 +1,80 @@
 // Package movingmedian computes the median of a windowed stream of data.
 package movingmedian
 
-import (
-	"container/heap"
-	"fmt"
-)
+import "sort"
 
-type item struct {
-	f         float64
-	heapIndex int
-}
-
-type itemHeap []*item
-
-func (h itemHeap) Len() int { return len(h) }
-func (h itemHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-	h[i].heapIndex = i
-	h[j].heapIndex = j
-}
-
-func (h *itemHeap) Push(x interface{}) {
-	e, ok := x.(*item)
-	if !ok {
-		panic(fmt.Sprintf("%T is not an *item", x)) //nolint:forbidigo
-	}
-	e.heapIndex = len(*h)
-	*h = append(*h, e)
-}
-
-func (h *itemHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-type minItemHeap struct {
-	itemHeap
-}
-
-func (h minItemHeap) Less(i, j int) bool { return h.itemHeap[i].f < h.itemHeap[j].f }
-
-type maxItemHeap struct {
-	itemHeap
-}
-
-func (h maxItemHeap) Less(i, j int) bool { return h.itemHeap[i].f > h.itemHeap[j].f }
-
-// MovingMedian computes the moving median of a windowed stream of numbers.
+// MovingMedian computes the median over a fixed-size sliding window.
+// It uses a ring buffer to track insertion order and a sorted slice for median queries.
 type MovingMedian struct {
-	queueIndex int
-	nitems     int
-	queue      []item
-	maxHeap    maxItemHeap
-	minHeap    minItemHeap
+	size   int       // fixed window size
+	ring   []float64 // ring buffer in insertion order
+	sorted []float64 // sorted copy of current window
+	pos    int       // next position in ring buffer to write
+	count  int       // number of elements currently in the window (<= size)
 }
 
-// NewMovingMedian returns a MovingMedian with the given window size.
-func NewMovingMedian(size int) MovingMedian {
-	m := MovingMedian{
-		queue:   make([]item, size),
-		maxHeap: maxItemHeap{make([]*item, 0, size/2+1)}, // Pre-allocate with capacity
-		minHeap: minItemHeap{make([]*item, 0, size/2+1)},
+// NewMovingMedian creates a new MovingMedian for a window of the given size.
+func NewMovingMedian(size int) *MovingMedian {
+	return &MovingMedian{
+		size:   size,
+		ring:   make([]float64, size),
+		sorted: make([]float64, 0, size),
 	}
-	heap.Init(&m.maxHeap)
-	heap.Init(&m.minHeap)
-	return m
 }
 
-// Push adds an element to the stream, removing old data which has expired from the window.  It runs in O(log windowSize).
+// Push adds a new observation. If the window is full, it removes the oldest.
 func (m *MovingMedian) Push(v float64) {
-	// Special case optimization for size 1
-	if len(m.queue) == 1 {
-		m.queue[0].f = v
-		return
-	}
+	if m.count < m.size {
+		// Window not yet full: add value to ring buffer and sorted slice.
+		m.ring[m.pos] = v
+		m.pos = (m.pos + 1) % m.size
 
-	// Use pointer arithmetic for queue management
-	itemPtr := &m.queue[m.queueIndex]
-	m.queueIndex++
-	if m.queueIndex >= len(m.queue) {
-		m.queueIndex = 0
-	}
+		// Insert v into the sorted slice.
+		i := sort.Search(m.count, func(i int) bool {
+			return m.sorted[i] >= v
+		})
+		m.sorted = append(m.sorted, 0)     // extend slice by one
+		copy(m.sorted[i+1:], m.sorted[i:]) // shift right
+		m.sorted[i] = v
+		m.count++
+	} else {
+		// Window is full: remove the oldest value then insert the new one.
+		old := m.ring[m.pos]
+		m.ring[m.pos] = v
+		m.pos = (m.pos + 1) % m.size
 
-	// Update value
-	itemPtr.f = v
-
-	// Fast path for initial window filling
-	if m.nitems < len(m.queue) {
-		m.nitems++
-
-		// Simple balancing for initial fill
-		if m.minHeap.Len() == 0 || v > m.minHeap.itemHeap[0].f {
-			heap.Push(&m.minHeap, itemPtr)
-			if m.minHeap.Len() > m.maxHeap.Len()+1 {
-				moveItem := heap.Pop(&m.minHeap)
-				heap.Push(&m.maxHeap, moveItem)
-			}
-			return
+		// Remove one occurrence of old from the sorted slice.
+		i := sort.Search(m.count, func(i int) bool {
+			return m.sorted[i] >= old
+		})
+		// There may be duplicates; we expect sorted[i]==old.
+		if i < m.count && m.sorted[i] == old {
+			copy(m.sorted[i:], m.sorted[i+1:])
+			m.sorted = m.sorted[:m.count-1]
+		} else {
+			// Should never happen.
+			panic("old element not found in sorted slice")
 		}
 
-		heap.Push(&m.maxHeap, itemPtr)
-		if m.maxHeap.Len() > m.minHeap.Len()+1 {
-			moveItem := heap.Pop(&m.maxHeap)
-			heap.Push(&m.minHeap, moveItem)
-		}
-		return
+		// Insert new value v into the sorted slice.
+		j := sort.Search(m.count-1, func(i int) bool {
+			return m.sorted[i] >= v
+		})
+		m.sorted = append(m.sorted, 0)
+		copy(m.sorted[j+1:], m.sorted[j:])
+		m.sorted[j] = v
 	}
-
-	// Main path for full window updates
-	minAbove := m.minHeap.itemHeap[0].f
-	maxBelow := m.maxHeap.itemHeap[0].f
-
-	// Check if item is in min heap
-	if itemPtr.heapIndex < m.minHeap.Len() && itemPtr == m.minHeap.itemHeap[itemPtr.heapIndex] {
-		if v >= maxBelow {
-			heap.Fix(&m.minHeap, itemPtr.heapIndex)
-			return
-		}
-		rotate(&m.maxHeap, &m.minHeap, m.maxHeap.itemHeap, m.minHeap.itemHeap, itemPtr)
-		return
-	}
-
-	// Item must be in max heap
-	if v <= minAbove {
-		heap.Fix(&m.maxHeap, itemPtr.heapIndex)
-		return
-	}
-	rotate(&m.minHeap, &m.maxHeap, m.minHeap.itemHeap, m.maxHeap.itemHeap, itemPtr)
 }
 
-func rotate(heapA, heapB heap.Interface, itemHeapA, itemHeapB itemHeap, itemPtr *item) {
-	moveItem := itemHeapA[0]
-	moveItem.heapIndex = itemPtr.heapIndex
-	itemHeapB[itemPtr.heapIndex] = moveItem
-	itemHeapA[0] = itemPtr
-	heap.Fix(heapB, itemPtr.heapIndex)
-	itemPtr.heapIndex = 0
-	heap.Fix(heapA, 0)
-}
-
-// Median returns the current value of the median from the window.
+// Median returns the current median of the window.
+// For an odd count, it returns the middle element; for an even count, the average of the two.
 func (m *MovingMedian) Median() float64 {
-	if len(m.queue) == 1 {
-		return m.queue[0].f
+	if m.count == 0 {
+		return 0.0
 	}
-
-	minLen := m.minHeap.Len()
-	maxLen := m.maxHeap.Len()
-
-	if maxLen == minLen {
-		if maxLen == 0 {
-			return 0.0
-		}
-		return (m.maxHeap.itemHeap[0].f + m.minHeap.itemHeap[0].f) / 2
+	if m.count%2 == 1 {
+		return m.sorted[m.count/2]
 	}
-
-	if maxLen > minLen {
-		return m.maxHeap.itemHeap[0].f
-	}
-
-	return m.minHeap.itemHeap[0].f
+	// For even count, average the two middle values.
+	return (m.sorted[m.count/2-1] + m.sorted[m.count/2]) / 2.0
 }
